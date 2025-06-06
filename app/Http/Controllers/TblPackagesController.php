148a592270c098b7eae5d863b7690b62490bec32
jakeeviado <jakematthewviado204@gmail.com>
2025-06-06 17:06:34 +0800

    package controller

--- app/Http/Controllers/TblPackagesController.php
+++ app/Http/Controllers/TblPackagesController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\tblPackages;
 use Illuminate\Http\Request;
 
 class TblPackagesController extends Controller
 {
-    //
+    public function fetchAllPackages(Request $request)
+    {
+        $packages = tblPackages::findAll();
+        return view('views.homepage')->with('employees', $packages);
+    }
+
+    public function fetchPackageById(Request $request, $id)
+    {
+        $package = tblPackages::find($id);
+        if ($package) {
+            return view('views.productdescriptionpage')->with('package', $package);
+        } else {
+            return redirect()->back()->with('error', 'Package not found');
+        }
+    }
 }
