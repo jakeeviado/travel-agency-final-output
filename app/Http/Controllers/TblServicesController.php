5ffcdc38ae41549c1e00232c9ce470d890caa569
jakeeviado <jakematthewviado204@gmail.com>
2025-06-06 12:20:46 +0800

    controllers

--- app/Http/Controllers/TblServicesController.php
+++ app/Http/Controllers/TblServicesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\tbl_services;
 use Illuminate\Http\Request;
 
 class TblServicesController extends Controller
 {
-    //
+    public function index()
+    {
+        $services = tbl_services::all();
+        return view('views.homepage', compact('services'));
+    }
 }
