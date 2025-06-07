c0b955880fcee4184e924003c690d184f912c83f
jakeeviado <jakematthewviado204@gmail.com>
2025-06-07 16:30:52 +0800

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [TblPackagesController::class, 'fetchAllPackages'])->name('packages.index');
 
 Route::get('/packages/{package_id}', [TblPackagesController::class, 'fetchPackageById'])->name('package.show');
-<<<<<<< HEAD
-=======
 
 Route::get('/aboutus', function () {
     return view('aboutus');
@@ -20,4 +18,3 @@ Route::get('/aboutus', function () {
 Route::get('/destinations', function () {
     return view('productspage');
 });
->>>>>>> master
