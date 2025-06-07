f915fd3a8eac5177f01ac4ffb06f9db238a645e3
jakeeviado <jakematthewviado204@gmail.com>
2025-06-07 12:55:18 +0800

    routes

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<footer class="footer">
+    <div class="social-icons">
+        <a href=""><img src="{{ asset('images/facebook.png') }}" alt="Facebook"></a>
+        <a href=""><img src="{{ asset('images/instagram.png') }}" alt="Instagram"></a>
+        <a href=""><img src="{{ asset('images/email.png') }}" alt="Email"></a>
+    </div>
+</footer>
