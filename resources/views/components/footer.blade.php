f103355acc76b76c05bd89cb83096b8b53274a31
ES-Aguilar <ela.shaina.aguilar@gmail.com>
2025-06-07 23:03:23 +0800

    add package seeder/update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -8,4 +8,18 @@
         <a href=""><img src="{{ asset('images/instagram.png') }}" alt="Instagram"></a>
         <a href=""><img src="{{ asset('images/email.png') }}" alt="Email"></a>
     </div>
+    <style>
+        .footer {
+    display: flex;
+    justify-content: flex-end; 
+    align-items: center;       
+    padding: 1rem 2rem;
+    border-top: 1px solid #ccc; 
+}
+
+    .social-icons {
+    display: flex;
+    gap: 15px;
+}
+    </style>
 </footer>
